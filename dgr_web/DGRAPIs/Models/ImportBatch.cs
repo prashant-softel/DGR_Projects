3fbcb3e1dbade437be92d4da62caa04fbb48181f
SoftelTechnology <info@softeltech.in>
2022-12-13 10:01:56 +0530

    code

---
+++ dgr_web/DGRAPIs/Models/ImportBatch.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DGRAPIs.Models
+{
+    public class ImportBatch
+    {
+        public string importFilePath { get; set; }
+        public int importSiteId { get; set; }
+        public string importType { get; set; }
+        public string importLogName { get; set; }
+    }
+
+    public class BatchIdImport
+    { 
+        public int import_batch_id{ get; set; }
+    }
+}
